751df72e9826edac8fbe7516c30a770e70d5f055
David LaBissoniere <david@labisso.com>
2013-07-24 15:21:52 -0500

    Added tests for offline storage

--- src/main/java/org/dasein/cloud/test/StatelessTestSuite.java
+++ src/main/java/org/dasein/cloud/test/StatelessTestSuite.java
@@ -40,6 +40,7 @@ import org.dasein.cloud.test.platform.StatelessMonitoringTests;
 import org.dasein.cloud.test.platform.StatelessNotificationsTests;
 import org.dasein.cloud.test.platform.StatelessRDBMSTests;
 import org.dasein.cloud.test.storage.StatelessObjectStoreTests;
+import org.dasein.cloud.test.storage.StatelessOfflineStoreTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -52,6 +53,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         StatelessObjectStoreTests.class,
+        StatelessOfflineStoreTests.class,
         StatelessAuthenticationTests.class,
         StatelessDCTests.class,
         StatelessVMTests.class,
