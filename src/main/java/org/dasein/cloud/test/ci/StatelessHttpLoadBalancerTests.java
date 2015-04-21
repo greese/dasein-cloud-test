bb4246acdd9a4e7c4c92b8d8778d5f763fc1412e
Roger Unwin <roger.unwin@enstratius.com>
2015-04-20 21:23:47 -0700

    misc polish

--- src/main/java/org/dasein/cloud/test/ci/StatelessHttpLoadBalancerTests.java
+++ src/main/java/org/dasein/cloud/test/ci/StatelessHttpLoadBalancerTests.java
@@ -150,10 +150,10 @@ public class StatelessHttpLoadBalancerTests {
                     Map<String, String> pathMap = new HashMap<String, String>();
                     pathMap.put("/*", "roger-bes-name");
                     pathMap.put("/video, /video/*", "roger-bes2-name");
+                    pathMap.put("/audio, /audio/*", "roger-bes2-name");
                     ConvergedHttpLoadBalancer withExperimentalConvergedHttpLoadbalancerOptions = ConvergedHttpLoadBalancer
                             .getInstance("roger-name", "roger-description", "roger-bes-name")
-                            .withHealthCheck("roger-hc-1", "roger-hc-1", null, 80, "/", 5, 5, 2, 2)
-                            //.withHealthCheck("roger-hc-2", "roger-hc-2", null, 80, "/", 5, 5, 2, 2) //ONLY ONE ALLOWED
+                            .withHealthCheck("roger-hc-1", "roger-hc-1", null, 80, "/", 5, 5, 2, 2) //ONLY ONE ALLOWED
                             .withBackendService("roger-bes-name", "roger-bes-description", 80, "http", "HTTP", new String[] {"roger-hc-1"}, 30)  // needs to know the backendservicebackend
                             .withBackendService("roger-bes2-name", "roger-bes2-description", 80, "http", "HTTP", new String[] {"roger-hc-1"}, 30) // needs to know the backendservicebackend
                             .withUrlSet("roger-url-map", "roger-url-map", "*", pathMap)
